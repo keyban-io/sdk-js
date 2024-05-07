350941b6b6184767bf7e5b0d4e80d3d3ab61f575
Ahmed Souissi <90756645+ahmdssi@users.noreply.github.com>
2024-05-07 11:14:10 +0200

    #82 init eddsa signer rust server

---
+++ signers/eddsa-server/src/main.rs
@@ -0,0 +1,18 @@
+use axum::{Router, http::StatusCode};
+use axum::routing::{get};
+
+
+async fn health() -> Result<String, StatusCode> {
+  Ok("Ok!".to_string())
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/health", get(health));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
+
+    axum::serve(listener, app).await.unwrap();
+}
