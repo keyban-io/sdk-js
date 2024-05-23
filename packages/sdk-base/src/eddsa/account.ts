8ec236e4bd8df82d181b3532c58849f00ad4d973
RadekKeyban <rwyszynski.ext@keyban.io>
2024-05-23 10:00:51 +0200

    Finalize custom monorepo setup

--- packages/sdk-base/src/eddsa/account.ts
+++ packages/sdk-base/src/eddsa/account.ts
@@ -1,43 +1,43 @@
-import type { ClientShare } from '~/eddsa/account.types';
-import type { WasmApi } from '~/eddsa/types';
+import type { ClientShare } from "~/eddsa/account.types";
+import type { WasmApi } from "~/eddsa/types";
 
 export class EddsaAccount {
-	/** Interface offering the WebAssembly Rust logic following {@link WasmApi} */
-	wasmApi;
-	/** Account address retrieved from ${@link clientKeyShare} */
-	address;
-	/** The client key share retrieved from storage. */
-	private clientKeyShare;
+  /** Interface offering the WebAssembly Rust logic following {@link WasmApi} */
+  wasmApi;
+  /** Account address retrieved from ${@link clientKeyShare} */
+  address;
+  /** The client key share retrieved from storage. */
+  private clientKeyShare;
 
-	/**
-	 * The constructor of the `EddsaClient` class.
-	 * @param wasmApi - The source of the WebAssembly Rust logic, for web is plain WebAssembly object, for react-native it required bridger
-	 * @param clientKeyShare - The client key share retrieved from storage.
-	 */
-	constructor(clientKeyShare: ClientShare, wasmApi: WasmApi) {
-		this.wasmApi = wasmApi;
-		this.clientKeyShare = clientKeyShare;
-		this.address = clientKeyShare;
-	}
+  /**
+   * The constructor of the `EddsaClient` class.
+   * @param wasmApi - The source of the WebAssembly Rust logic, for web is plain WebAssembly object, for react-native it required bridger
+   * @param clientKeyShare - The client key share retrieved from storage.
+   */
+  constructor(clientKeyShare: ClientShare, wasmApi: WasmApi) {
+    this.wasmApi = wasmApi;
+    this.clientKeyShare = clientKeyShare;
+    this.address = clientKeyShare;
+  }
 
-	async signPayload(payload: Record<string, unknown>) {
-		const wasmReadyPayload = this.prepareWasmPayload(payload);
+  async signPayload(payload: Record<string, unknown>) {
+    const wasmReadyPayload = this.prepareWasmPayload(payload);
 
-		const signature = await this.wasmApi.signMessage(
-			this.clientKeyShare,
-			wasmReadyPayload,
-		);
+    const signature = await this.wasmApi.signMessage(
+      this.clientKeyShare,
+      wasmReadyPayload
+    );
 
-		return signature;
-	}
+    return signature;
+  }
 
-	prepareWasmPayload(payload: Record<string, unknown>) {
-		// Not sure how the payload should be prepared
-		return JSON.stringify(payload);
-	}
+  prepareWasmPayload(payload: Record<string, unknown>) {
+    // Not sure how the payload should be prepared
+    return JSON.stringify(payload);
+  }
 
-	// FOR TESTING ONLY
-	async add(num1: number, num2: number) {
-		return this.wasmApi.add(num1, num2);
-	}
+  // FOR TESTING ONLY
+  async add(num1: number, num2: number) {
+    return this.wasmApi.add(num1, num2);
+  }
 }
--- packages/sdk-base/src/eddsa/types.ts
+++ packages/sdk-base/src/eddsa/types.ts
@@ -2,14 +2,13 @@ export type Hex = string;
 
 // All return types should be a promise, even it wasm methods are sync
 // the reason is that react native will promisify them anyway
-
 export type WasmApi = {
-	signMessage: (address: string, payload: string) => Promise<Hex>;
-	generateKeypair: () => Promise<unknown>;
-	add: (n1: number, n2: number) => Promise<number>;
+  signMessage: (address: string, payload: string) => Promise<Hex>;
+  generateKeypair: () => Promise<unknown>;
+  add: (n1: number, n2: number) => Promise<number>;
 };
 
 export type StorageProviderApi = {
-	save: (key: string, payload: string) => Promise<boolean>;
-	get: (key: string) => Promise<string | undefined>;
+  save: (key: string, payload: string) => Promise<boolean>;
+  get: (key: string) => Promise<string | undefined>;
 };
