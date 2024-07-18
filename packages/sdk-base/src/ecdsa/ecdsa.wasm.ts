ae816496a8fabe7940c08a90758d2da69b5d4054
RadekKeyban <rwyszynski.ext@keyban.io>
2024-07-18 12:11:34 +0200

    Fix cors issue for ecdsa server

--- packages/sdk-base/src/ecdsa/ecdsa.wasm.ts
+++ packages/sdk-base/src/ecdsa/ecdsa.wasm.ts
@@ -1,14 +1,14 @@
 import initWasmFile from 'ecdsa-wasm-client';
-import type { WasmApi } from '~/eddsa';
+import type { EcdsaWasmApi } from './ecdsa.types';
 
-const initEcdsaWasm = async (): Promise<WasmApi> => {
+const initEcdsaWasm = async (): Promise<EcdsaWasmApi> => {
   await initWasmFile();
   // ts-ignore
   const ecdsa = (window as any).ecdsa;
   return {
     add: (...args) => Promise.resolve(Math.max(...args)),
     signMessage: ecdsa.sign,
-    dkg: ecdsa.dkg as WasmApi['dkg'],
+    dkg: ecdsa.dkg as EcdsaWasmApi['dkg'],
   };
 };
 export { initEcdsaWasm };
