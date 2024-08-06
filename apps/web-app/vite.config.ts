3f2c6ccb6d6533c357983e223ebb90e140298c38
Samuel Loury <konubinixweb@gmail.com>
2024-08-06 11:51:53 +0200

    refactor: Use the docker images of the web-app and demo-app by default
    To have our dev env look as much as possible like the production
    one. One working with the demo can still use --dev-demo to go back
    to the live one.
    
    Also, in order to ease moving from one image to the other, all the listened
    ports are now the same between web/demo app and docker/live images. It is now
    4200 because, well, why not!?

--- apps/web-app/vite.config.ts
+++ apps/web-app/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   server: {
     host: true,
-    port: 3005,
+    port: 4200,
   },
   preview: {
     host: true,
