53a5b865d1775c4240da1e1856dcaee871ba57f7
Sebastien Dolard <sdolard@keyban.io>
2024-08-21 18:44:18 +0200

    chore: Update KeybanSigner documentation

--- packages/sdk-base/src/signer/index.ts
+++ packages/sdk-base/src/signer/index.ts
@@ -2,6 +2,14 @@ import type { Hex } from "viem";
 import { KeybanSigner_ECDSA } from "./ecdsa";
 import { KeybanSigner_EdDSA } from "./eddsa";
 
+/**
+ * Interface for the Keyban signer.
+ * This interface defines the methods that a Keyban signer must implement.
+ * The signer is responsible for generating and signing keys.
+ * The signer is also responsible for generating the public key from the client share.
+ * The signer is used by the Keyban client to sign messages and generate public keys.
+ * @private
+ */
 export interface KeybanSigner<Share> {
   storagePrefix: string;
   dkg(keyId: string, apiUrl: string): Promise<Share>;
@@ -14,6 +22,9 @@ export interface KeybanSigner<Share> {
   publicKey(clientShare: Share): Promise<Hex>;
 }
 
+/**
+ * Object that contains the Keyban signers.
+ */
 export const KeybanSigner = {
   EdDSA: KeybanSigner_EdDSA,
   ECDSA: KeybanSigner_ECDSA,
