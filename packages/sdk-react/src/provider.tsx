88372479f6971b8b8091b0e704a183bf3234765b
Sebastien Dolard <sdolard@keyban.io>
2024-08-21 18:43:21 +0200

    chore: Update KeybanProvider documentation

--- packages/sdk-react/src/provider.tsx
+++ packages/sdk-react/src/provider.tsx
@@ -12,10 +12,38 @@ export type KeybanContextType = {
 };
 const KeybanContext = React.createContext<KeybanContextType | null>(null);
 
+/**
+ * @private
+ */
 export type KeybanProviderProps<Share> = React.PropsWithChildren<
   KeybanClientConfig<Share>
 >;
 
+/**
+ * Provider component for the Keyban SDK.
+ * This component must be used to wrap the application and provide the Keyban SDK
+ * functionalities to the components.
+ *
+ * @returns The provider component.
+ *
+ * @see {@link KeybanClientConfig} for the configuration options.
+ *
+ * @example
+ * ```tsx
+ * import { KeybanProvider, KeybanChain, KeybanSigner, KeybanLocalStorage } from "@keyban/sdk-react";
+ *
+ * // Main Wallet component wrapped with KeybanProvider to supply SDK context
+ * const Wallet: React.FC = () => (
+ *   <KeybanProvider
+ *     chain={KeybanChain.polygonAmoy} // Specifies the blockchain network (Polygon)
+ *     signer={KeybanSigner.ECDSA} // Specifies the signing algorithm (ECDSA)
+ *     storage={KeybanLocalStorage} // Specifies the storage mechanism (LocalStorage)
+ *   >
+ *     <WalletContent />  // WalletContent is a component that uses the Keyban SDK
+ *   </KeybanProvider>
+ * );
+ * ```
+ */
 export function KeybanProvider<Share>({
   children,
   ...KeybanClientConfig
@@ -45,7 +73,10 @@ export function KeybanProvider<Share>({
     <KeybanContext.Provider value={value}>{children}</KeybanContext.Provider>
   );
 }
-
+/**
+ * Hook to access the Keyban SDK functionalities within a component
+ * @throws Error if the hook is used outside of a {@link KeybanProvider}
+ */
 export const useKeyban = () => {
   const ctx = React.useContext(KeybanContext);
   if (!ctx)
