dd713bd1ccefaf2368bff961f2c852c49c0919cd
Sebastien Dolard <sdolard@keyban.io>
2024-08-26 16:35:18 +0200

    refactor: Update EnvSelector component to use MUI components and improve code structure

--- apps/demo-app/src/components/EnvSelector.tsx
+++ apps/demo-app/src/components/EnvSelector.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "@emotion/styled";
-import { Tooltip } from "@mui/material";
+import { Select, MenuItem, FormControl, FormHelperText } from "@mui/material";
 
 interface Env {
   id: string;
@@ -14,57 +14,28 @@ interface EnvSelectorProps {
   onSelectEnv: (envId: string) => void;
 }
 
-const SelectorContainer = styled.div`
-  display: flex;
-  align-items: center;
-  padding: 10px;
-  background-color: var(--container-background-color);
-  border: 1px solid var(--border-color);
-  border-radius: var(--border-radius);
-  margin-bottom: 20px; /* Espacement des éléments */
-`;
-
-const Label = styled.label`
-  margin-right: 10px;
-  font-size: 1em;
-  color: var(--primary);
-`;
-
-const Select = styled.select`
-  padding: 5px;
-  border: 1px solid var(--border-color);
-  border-radius: var(--border-radius);
-  background-color: #fff;
-  color: var(--primary);
-  outline: none;
-
-  &:focus {
-    border-color: var(--primary-hover-color);
-  }
-`;
-
 const EnvSelector: React.FC<EnvSelectorProps> = ({
   envs,
   selectedEnvId,
   onSelectEnv,
 }) => {
   return (
-    <SelectorContainer>
-      <Tooltip title="Select Env" arrow>
-        <Label htmlFor="env-select">Env:</Label>
-      </Tooltip>
+    <FormControl fullWidth size="small">
       <Select
+        variant="standard"
         id="env-select"
+        label="Env"
         value={selectedEnvId}
         onChange={(e) => onSelectEnv(e.target.value)}
       >
         {envs.map((envs) => (
-          <option key={envs.id} value={envs.id}>
+          <MenuItem key={envs.id} value={envs.id}>
             {envs.name}
-          </option>
+          </MenuItem>
         ))}
       </Select>
-    </SelectorContainer>
+      <FormHelperText>Env</FormHelperText>
+    </FormControl>
   );
 };
 
