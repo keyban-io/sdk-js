8d066beb6326caa9f706a7cb11b6aa4d7a2d2ab9
Sebastien Dolard <sdolard@keyban.io>
2024-08-26 16:51:29 +0200

    refactor: Update BalanceInfo component to use MUI components and improve code structure

--- apps/demo-app/src/components/BalanceInfo.tsx
+++ apps/demo-app/src/components/BalanceInfo.tsx
@@ -1,7 +1,6 @@
 import type React from "react";
 import { FormattedBalance } from "@keyban/sdk-react";
-import styled from "@emotion/styled";
-import { Tooltip } from "@mui/material";
+import { Button, Stack, Tooltip, Typography } from "@mui/material";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 
@@ -11,83 +10,36 @@ interface BalanceInfoProps {
   onSend: () => void;
 }
 
-const BalanceContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 10px;
-  background-color: var(--container-background-color);
-  border: 1px solid var(--border-color);
-  border-radius: var(--border-radius);
-  transition: background-color 0.3s ease;
-
-  &:hover {
-    background-color: #f9f9f9;
-  }
-`;
-
-const BalanceDetails = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-bottom: 10px;
-`;
-
-const Balance = styled.span`
-  font-size: 1.2em;
-  margin-bottom: 5px;
-`;
-
-const EuroBalance = styled.span`
-  font-size: 1em;
-  color: var(--primary);
-`;
-
-const SendButton = styled.button`
-  background: none;
-  border: none;
-  color: var(--primary);
-  cursor: pointer;
-  font-size: 1.2em;
-  display: flex;
-  align-items: center;
-  margin-top: 10px;
-
-  &:hover {
-    color: var(--primary-hover-color);
-  }
-
-  .fa {
-    margin-left: 5px;
-  }
-`;
-
 const BalanceInfo: React.FC<BalanceInfoProps> = ({
   balance,
   euroBalance,
   onSend,
 }) => {
   return (
-    <BalanceContainer>
-      <BalanceDetails>
-        <Tooltip title="Cryptocurrency Balance" arrow>
-          <Balance>
-            {balance != null && <FormattedBalance balance={balance} />}
-          </Balance>
-        </Tooltip>
-        <Tooltip title="Equivalent Euro Balance" arrow>
-          <EuroBalance>
-            {euroBalance != null && <span>€{euroBalance.toFixed(2)}</span>}
-          </EuroBalance>
-        </Tooltip>
-      </BalanceDetails>
-      <Tooltip title="Send" arrow>
-        <SendButton type="button" onClick={onSend}>
-          Send
-          <FontAwesomeIcon className="fa" icon={faPaperPlane} />
-        </SendButton>
+    <Stack
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      spacing={1}
+    >
+      <Tooltip title="Cryptocurrency Balance" arrow>
+        <Typography variant="h5">
+          {balance != null && <FormattedBalance balance={balance} />}
+        </Typography>
       </Tooltip>
-    </BalanceContainer>
+      <Tooltip title="Equivalent Euro Balance" arrow>
+        <Typography variant="body1">
+          {euroBalance != null && <span>€{euroBalance.toFixed(2)}</span>}
+        </Typography>
+      </Tooltip>
+      <Button
+        variant="contained"
+        onClick={onSend}
+        startIcon={<FontAwesomeIcon className="fa" icon={faPaperPlane} />}
+      >
+        Send
+      </Button>
+    </Stack>
   );
 };
 
