600a3e5e48a07b54036a3aae6208bb994d72fdc6
Sebastien Dolard <sdolard@keyban.io>
2024-08-27 17:10:21 +0200

    refactor: Update QRCodePage component to use MUI components and improve code structure

--- apps/demo-app/src/pages/qr-code.tsx
+++ apps/demo-app/src/pages/qr-code.tsx
@@ -1,70 +1,9 @@
-import React from "react";
+import type React from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import QRCode from "qrcode.react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBell } from "@fortawesome/free-solid-svg-icons";
-import styled from "@emotion/styled";
-
-const WalletDashboard = styled.div`
-  padding: 20px;
-  background-color: var(--primary-lightest);
-  border: 1px solid var(--border-color);
-  border-radius: var(--border-radius);
-  max-width: 600px;
-  margin: 0 auto;
-  text-align: center;
-`;
-
-const Header = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 10px 0;
-  border-bottom: 1px solid var(--border-color);
-  margin-bottom: 20px;
-`;
-
-const Section = styled.div`
-  margin-top: 20px;
-  text-align: center;
-`;
-
-const Notification = styled.span`
-  cursor: pointer;
-  color: var(--primary);
-`;
-
-const BackButton = styled.button`
-  margin-top: 20px;
-  background-color: var(--primary);
-  color: white;
-  border: none;
-  border-radius: 4px;
-  padding: 10px 20px;
-  cursor: pointer;
-  display: block;
-  margin: 20px auto;
-  min-width: 30%;
-
-  &:hover {
-    background-color: var(--primary-hover-color);
-  }
-`;
-
-const AddressText = styled.h2`
-  margin-bottom: 20px;
-  word-wrap: break-word;
-  font-size: 1.2em;
-  color: var(--primary);
-`;
-
-const QRCodeContainer = styled.div`
-  padding: 20px;
-  background-color: white;
-  border: 1px solid var(--border-color);
-  border-radius: var(--border-radius);
-  display: inline-block;
-`;
+import { Stack, Typography, IconButton, Button } from "@mui/material";
 
 const QRCodePage: React.FC = () => {
   const navigate = useNavigate();
@@ -76,27 +15,33 @@ const QRCodePage: React.FC = () => {
   };
 
   return (
-    <WalletDashboard>
-      <Header>
-        <span>Keyban WAAS Demo</span>
-        <Notification>
+    <Stack spacing={2}>
+      <Stack direction="row" justifyContent="space-between">
+        <Typography variant="h4">Keyban WAAS Demo</Typography>
+        <IconButton color="primary">
           <FontAwesomeIcon icon={faBell} />
-        </Notification>
-      </Header>
-      <Section>
-        <AddressText>{address || "No address provided"}</AddressText>
-        {address ? (
-          <QRCodeContainer>
-            <QRCode value={address} size={256} level="H" />
-          </QRCodeContainer>
-        ) : (
-          <p>Loading address...</p>
-        )}
-        <BackButton type="button" onClick={handleBackClick}>
+        </IconButton>
+      </Stack>
+      <Stack
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        spacing={4}
+      >
+        <Typography variant="h6">
+          {address
+            ? "Share this address with your friends scan the QR code."
+            : null}
+        </Typography>
+        <Typography variant="body1">
+          {address || "No address provided"}
+        </Typography>
+        {address ? <QRCode value={address} size={256} level="H" /> : null}
+        <Button variant="contained" onClick={handleBackClick}>
           Back to Dashboard
-        </BackButton>
-      </Section>
-    </WalletDashboard>
+        </Button>
+      </Stack>
+    </Stack>
   );
 };
 
