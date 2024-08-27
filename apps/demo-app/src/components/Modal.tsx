a248e93ac76686cc2521c874ad34aea2899e4644
Sebastien Dolard <sdolard@keyban.io>
2024-08-27 21:49:20 +0200

    refactor: Update Modal component to import React as type to improve code structure

--- apps/demo-app/src/components/Modal.tsx
+++ apps/demo-app/src/components/Modal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import type React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClose } from "@fortawesome/free-solid-svg-icons";
 
