26401026b6c12f2e113227ecbe300dd9dbd319a9
Samuel Hurel <samuel.hurel@gmail.com>
2024-09-03 17:02:49 +0200

    feat: fid demo google login

--- apps/fid-demo/src/main.tsx
+++ apps/fid-demo/src/main.tsx
@@ -1,10 +1,24 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import App from "@/App.tsx";
-import "@/index.css";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 );
