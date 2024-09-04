1b4fa256bc0fa5352aab3c5581d5c645eaf6bc2a
Samuel Hurel <shure@keyban.io>
2024-09-04 09:40:32 +0200

    chore: rename fid-demo -> loyalty-demo

--- apps/loyalty-demo/src/App.tsx
+++ apps/loyalty-demo/src/App.tsx
@@ -49,7 +49,7 @@ export default function App() {
 
   return (
     <Container fixed>
-      <Typography variant="h1">Fid Demo</Typography>
+      <Typography variant="h1">Loyalty Demo</Typography>
 
       {error && (
         <Alert severity="error">
