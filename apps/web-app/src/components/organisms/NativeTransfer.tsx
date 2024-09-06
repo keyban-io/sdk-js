6218f3205aa0df2369a262cbc617132aa39b0962
Ahmed Souissi <90756645+ahmdssi@users.noreply.github.com>
2024-09-06 16:57:28 +0200

    feat(web-app) display formatted transfer value

--- apps/web-app/src/components/organisms/NativeTransfer.tsx
+++ apps/web-app/src/components/organisms/NativeTransfer.tsx
@@ -2,7 +2,7 @@ import Row from "@/components/atoms/Row";
 import SerializedValue from "@/components/atoms/SerializedValue";
 import BigIntField from "@/components/molecules/BigIntField";
 import TextField from "@/components/molecules/TextField";
-import { Address, useKeybanAccount } from "@keyban/sdk-react";
+import { Address, FormattedBalance, useKeybanAccount } from "@keyban/sdk-react";
 import React from "react";
 
 export type NativeTransferProps = { keyId: string };
@@ -21,19 +21,27 @@ export default function NativeTransfer({ keyId }: NativeTransferProps) {
 
       <Row>
         <BigIntField
-          label="Value"
+          label="Raw value"
           value={value?.toString()}
           onChange={setValue}
           data-test-id="NativeTransfer:value"
         />
+        <span>Formatted:</span>
+        <div data-test-id="NativeTransfer:formattedValue">
+          <FormattedBalance balance={value} />
+        </div>
+      </Row>
 
+      <Row>
         <TextField
           label="Recipient"
           value={recipient}
           onChange={setRecipient}
           data-test-id="NativeTransfer:recipient"
         />
+      </Row>
 
+      <Row>
         <button
           type="button"
           onClick={() =>
@@ -47,12 +55,15 @@ export default function NativeTransfer({ keyId }: NativeTransferProps) {
           Transfer
         </button>
       </Row>
+      <Row>
+        <span>Hash:</span>
+        <SerializedValue
+          value={hash}
+            style={{ flexGrow: 1 }}
+          data-test-id="NativeTransfer:hash"
+        />
+      </Row>
 
-      <SerializedValue
-        value={hash}
-        style={{ marginBlockStart: "0.5em" }}
-        data-test-id="NativeTransfer:hash"
-      />
     </fieldset>
   );
 }
