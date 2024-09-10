4afdc2711c80724fdae7066470d6a84c4d95fd21
Sebastien Dolard <sdolard@keyban.io>
2024-09-10 11:10:31 +0200

    feat: Add aliases for Ethereum address, hash, and hexadecimal value

--- packages/sdk-base/src/index.ts
+++ packages/sdk-base/src/index.ts
@@ -10,6 +10,17 @@ export { KeybanSigner } from './signer';
 export { KeybanStorage } from './storage';
 export * from "./utils";
 
+/**
+ * Alias for an Ethereum address. It must be a string that begins with "0x".
+ */
 export type Address = `0x${string}`;
+/**
+ * Alias for a hash value. It must be a string that begins with "0x".
+ * Commonly used to represent transaction or block hashes.
+ */
 export type Hash = `0x${string}`;
+/**
+ * Alias for a hexadecimal value. It must be a string that begins with "0x".
+ * Typically used to represent public keys or signed messages in blockchain-related contexts.
+ */
 export type Hex = `0x${string}`;
