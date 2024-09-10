c9b168d1c59b0e8735a6bc12129d42c9655a84ca
Sebastien Dolard <sdolard@keyban.io>
2024-09-10 10:20:42 +0200

    chore: comment update

--- packages/sdk-base/src/chains.ts
+++ packages/sdk-base/src/chains.ts
@@ -1,7 +1,7 @@
-import * as chains from "viem/chains";
-import { KeybanChain } from "~/index";
+import * as chains from 'viem/chains';
+import { KeybanChain } from '~/index';
 
-export { GqlChainType as KeybanChain } from "~/gql-types";
+export { GqlChainType as KeybanChain } from '~/gql-types';
 
 export const viemChainsMap: { [C in KeybanChain]: chains.Chain } = {
   [KeybanChain.KeybanTestnet]: chains.anvil,
