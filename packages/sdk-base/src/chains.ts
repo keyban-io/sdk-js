e2410bc3cde7f5c6d4be865b21d0f381ab9abe98
Samuel Hurel <samuel.hurel@gmail.com>
2024-09-16 19:00:44 +0200

    fix: sdk re-export of KeybanChain

--- packages/sdk-base/src/chains.ts
+++ packages/sdk-base/src/chains.ts
@@ -3,7 +3,7 @@ import * as chains from "viem/chains";
 import { type IKeybanSigner, KeybanSigner } from "~/signer";
 import { GqlChainType as KeybanChain } from "~/gql-types";
 
-export { type GqlChainType as KeybanChain } from "~/gql-types";
+export { GqlChainType as KeybanChain } from "~/gql-types";
 
 export const viemChainsMap: { [C in KeybanChain]: chains.Chain } = {
   [KeybanChain.KeybanTestnet]: chains.anvil,
