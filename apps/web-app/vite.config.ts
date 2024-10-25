8aa29a10315a89455508bf95fc88968e73c67fa3
Samuel Hurel <samuel.hurel@gmail.com>
2024-10-25 08:52:53 +0200

    feat: add grahpiql in web-app

--- apps/web-app/vite.config.ts
+++ apps/web-app/vite.config.ts
@@ -5,6 +5,14 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        graphiql: "graphiql.html",
+      },
+    },
+  },
   server: {
     host: true,
     port: 4200,
