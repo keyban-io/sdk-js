16b553fab8320a2ec859b04cd7c8b6d499a7f434
Sebastien Dolard <sdolard@keyban.io>
2024-11-25 19:52:42 +0100

    feat(doc-manager): update TypeDoc configuration and scripts for documentation generation

---
+++ apps/doc-manager/scripts/update-typedoc-options.js
@@ -0,0 +1,27 @@
+const fs = require("node:fs");
+const path = require("node:path");
+
+// Chemins des fichiers
+const originalPath = path.resolve(
+  __dirname,
+  "../../../../docs/docusaurus/typedocOptions.json",
+);
+const copyPath = path.resolve(
+  __dirname,
+  "../../../../docs/docusaurus/typedocOptions.temp.json",
+);
+
+// Charger le fichier original
+const options = JSON.parse(fs.readFileSync(originalPath, "utf8"));
+
+// Mise à jour des plugins
+options.plugin = options.plugin.map((plugin) =>
+  plugin === "typedoc-plugin-merge-modules"
+    ? "typedoc-plugin-markdown"
+    : plugin,
+);
+
+// Sauvegarde de la copie mise à jour
+fs.writeFileSync(copyPath, JSON.stringify(options, null, 2), "utf8");
+
+console.log(`Options mises à jour et sauvegardées dans ${copyPath}`);
