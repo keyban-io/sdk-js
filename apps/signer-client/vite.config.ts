9317c7c3bb94a727e59550a38651bcbb4f534586
Maxime Vanmeerbeck <maxime.vanmeerbeck@gmail.com>
2024-11-26 12:28:09 +0100

    init signer client app that will host the wasm

---
+++ apps/signer-client/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+// https://vitejs.dev/config/
+export default defineConfig((configEnv) => ({
+  plugins: [tsconfigPaths()],
+  server: {
+    host: true,
+    port: 4200,
+  }
+}));
