56d75f6f47b99517adb4e18b1ecbc1e7a60a0787
Ahmed Souissi <90756645+ahmdssi@users.noreply.github.com>
2024-11-26 14:11:56 +0100

    revert last "fix"

--- packages/sdk-base/src/signer/ecdsa.ts
+++ packages/sdk-base/src/signer/ecdsa.ts
@@ -1,22 +1,23 @@
+import initWasmFile from "@keyban/ecdsa-wasm-client";
+
 import { type IKeybanSigner } from "~/signer";
 import { WasmKeybanSigner } from "~/signer/wasm";
 
-import initWasmFile from "@keyban/ecdsa-wasm-client";
-
 export class KeybanSigner_ECDSA
   extends WasmKeybanSigner(initWasmFile)
-  implements IKeybanSigner {
+  implements IKeybanSigner
+{
   storagePrefix = "KEYBAN-ECDSA";
 
   dkg: IKeybanSigner["dkg"] = KeybanSigner_ECDSA.wrap((...args) =>
-    (globalThis as any).ecdsa.dkg(...args),
+    globalThis.ecdsa.dkg(...args),
   );
 
   sign: IKeybanSigner["sign"] = KeybanSigner_ECDSA.wrap((...args) =>
-    (globalThis as any).ecdsa.sign(...args),
+    globalThis.ecdsa.sign(...args),
   );
 
   publicKey: IKeybanSigner["publicKey"] = KeybanSigner_ECDSA.wrap((...args) =>
-    (globalThis as any).ecdsa.publicKey(...args),
+    globalThis.ecdsa.publicKey(...args),
   );
 }
