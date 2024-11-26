0c63557401d7ca3aac2b04a247188c8e78ea9731
Maxime Vanmeerbeck <maxime.vanmeerbeck@gmail.com>
2024-11-26 14:41:22 +0100

    signer client linting

--- apps/signer-client/vite.config.ts
+++ apps/signer-client/vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
-export default defineConfig((configEnv) => ({
+export default defineConfig(() => ({
   plugins: [tsconfigPaths()],
   server: {
     host: true,
     port: 4200,
-  }
+  },
 }));
