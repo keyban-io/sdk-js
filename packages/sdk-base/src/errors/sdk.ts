ab675155e49be68eeab89e029c81f34279f7ed67
Sebastien Dolard <sdolard@keyban.io>
2024-11-27 11:28:41 +0100

    refactor: enhance SDK error documentation and improve NFT error message clarity

--- packages/sdk-base/src/errors/sdk.ts
+++ packages/sdk-base/src/errors/sdk.ts
@@ -112,7 +112,6 @@ export class SdkError extends KeybanBaseError<SdkErrorTypes> {
   /**
    * The available SDK error types.
    *
-   * @static
    * @type {typeof SdkErrorTypes}
    */
   static types = SdkErrorTypes;
