f6d8a627abeca3a2a6a80d354a144ba8e1023412
Samuel Hurel <samuel.hurel@gmail.com>
2024-11-29 14:01:09 +0100

    chore: simplify jwt decode

--- apps/signer-client/src/utils/jwt.ts
+++ apps/signer-client/src/utils/jwt.ts
@@ -1,16 +1,12 @@
-// Unicode text JWT parser function
+import "core-js/actual/typed-array/from-base64";
 
 import { SignerClientError } from "~/errors/SignerClientError";
 
-// @see https://developer.mozilla.org/en-US/docs/Glossary/Base64#the_unicode_problem
 export function parseJwt(accessToken: string) {
   try {
     const base64 = accessToken.split(".")[1];
-    const bin = Uint8Array.from(
-      atob(base64)
-        .split("")
-        .map((m) => m.codePointAt(0)!),
-    );
+    // @ts-expect-error: Uint8Array.fromBase64 is polyfilled by core-js
+    const bin = Uint8Array.fromBase64(base64);
     const json = new TextDecoder().decode(bin);
 
     return JSON.parse(json);
