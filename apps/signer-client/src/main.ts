d58201cf62b1fbf921cd5f4b34184057decb2af8
Samuel Hurel <samuel.hurel@gmail.com>
2025-01-03 08:46:44 +0100

    chore: remove unused code

--- apps/signer-client/src/main.ts
+++ apps/signer-client/src/main.ts
@@ -3,10 +3,6 @@ import { RpcServer } from "@keyban/sdk-base/rpc";
 import { KeybanAuth } from "~/auth";
 import { KeybanSigner_ECDSA } from "~/signer/ecdsa";
 
-const url = new URL(window.location.href);
-const appId = url.searchParams.get("appId");
-if (!appId) throw new Error("Invalid appId");
-
 const auth = new KeybanAuth();
 const ecdsa = new KeybanSigner_ECDSA(auth);
 
