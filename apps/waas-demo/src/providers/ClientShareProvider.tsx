615bc1d4b23184fb4fd248302d0cd1701213906a
Sebastien Dolard <sdolard@keyban.io>
2025-01-09 11:05:34 +0100

    refactor: migrate ClientShareProvider to a new provider structure and implement CustomFetchBaseProvider for API interactions

---
+++ apps/waas-demo/src/providers/ClientShareProvider.tsx
@@ -0,0 +1,25 @@
+import CustomFetchBaseProvider from "./CustomFetchBaseProvider";
+
+class ClientShareProvider extends CustomFetchBaseProvider {
+  constructor(appId: string) {
+    super(`KEYBAN-CLIENT-SHARE:${appId}`);
+  }
+
+  // Retrieving data (GET)
+  async get() {
+    return this.fetch("/api/clientShare", {
+      method: "GET",
+    });
+  }
+
+  // Sending/Saving data (POST or PUT)
+  async set(clientShare: string) {
+    return this.fetch("/api/clientShare", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: clientShare,
+    });
+  }
+}
+
+export default ClientShareProvider;
