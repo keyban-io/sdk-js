a6eff0e688c7d856549c7ea415995463e2a6dd19
Sebastien Dolard <sdolard@keyban.io>
2025-01-28 11:28:06 +0100

    refactor: remove unused Box import in SignInWithLoginPasswordButton

--- packages/sdk-react/src/components/buttons/SignInWithLoginPasswordButton.tsx
+++ packages/sdk-react/src/components/buttons/SignInWithLoginPasswordButton.tsx
@@ -1,4 +1,3 @@
-import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Checkbox from "@mui/material/Checkbox";
 import FormControl from "@mui/material/FormControl";
