6ccecf153987aa48ae4c7d950535052ed7c04463
Antoine <asablayrolles@keyban.io>
2025-03-26 11:01:45 +0100

    add dpp short domain config

--- apps/dpp-app/src/config.ts
+++ apps/dpp-app/src/config.ts
@@ -5,6 +5,7 @@ const appId = "e7b8f9d2-3c4e-4f6e-9a7b-123456789def";
 const apiUrl = {
   "http://localhost:4200": "https://api.keyban.localtest.me/v1",
   "https://dpp-app.keyban.fr": "https://api.keyban.io/v1",
+  "https://dpp-app.prod.keyban.fr": "https://api.keyban.io/v1",
   "https://dpp-app.staging.keyban.fr": "https://api.staging.keyban.io/v1",
   "https://dpp-app.testing.keyban.fr": "https://api.testing.keyban.io/v1",
   "https://dpp-app.marc.lvh.me": "https://api.keyban.localtest.me/v1",
